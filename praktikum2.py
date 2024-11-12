cdd72f0651e1fd50e4b98f0897ac71a4632ad8a7
hamizanraden <hamizanraden@gmail.com>
2024-11-12 13:45:48 +0700

    menambahkan file praktikum2.py

---
+++ praktikum2.py
@@ -0,0 +1,17 @@
+#####################################
+#Nama  : Raden Hamizan Rizky Kusuma
+#NIM   : 2403657
+#Kelas : 1C
+#####################################
+
+
+def totalangka(*angka):
+    if len(angka) == 0:
+        return 0
+    total = sum(angka)
+    ratatara = total / len(angka)
+    return(f"nilai rata ratanya adalah {ratatara}")
+
+
+print(totalangka(2, 3, 5, 10))
+print(totalangka(5, 10))
